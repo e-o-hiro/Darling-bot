744004cc142110de5e1aa9df44af41ece7696be1
hiro <novaipandoficer@gmail.com>
2021-08-10 16:26:50 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,59 @@
+const help = (pushname, prefix) => {
+	return `
+┏━━━━━━━━━━━━━━━
+┠ ➥ | Darling Bot
+┠ ➥ | Prefix:「${prefix}」
+┠ ➥ | Status: Online
+┠ ➥ | By Hiro
+┗━━━━━━━━━━━━━
+
+┏━━━「Usuario」
+┠ ➥ | Nome: ${pushname}
+┠ ➥ | Registro: Nop
+┗━━━━━━━━━━━━━
+
+┏━━━「Grupos」
+┠ ➥ | ${prefix}add 55xxxxxxx
+┠ ➥ | ${prefix}welcome [1/0]
+┠ ➥ | ${prefix}ban @tagmember
+┠ ➥ | ${prefix}promover @tagmember
+┠ ➥ | ${prefix}rebaixar @tagmember
+┠ ➥ | ${prefix}marcar
+┠ ➥ | ${prefix}hidetag
+┠ ➥ | ${prefix}linkgp
+┠ ➥ | ${prefix}fotogp
+┠ ➥ | ${prefix}antifake
+┠ ➥ | ${prefix}sair
+┗━━━━━━━━━━━━━
+
+┏━━━「Figurinhas」
+┠ ➥ | ${prefix}sticker
+┠ ➥ | ${prefix}fig
+┠ ➥ | ${prefix}toimg
+┠ ➥ | ${prefix}attp
+┗━━━━━━━━━━━━━
+
+┏━━━「Jogos」
+┠ ➥ | ${prefix}rr
+┠ ➥ | ${prefix}abraço
+┗━━━━━━━━━━━━━
+
+┏━━━「Musica」
+┠ ➥ | ${prefix}play
+┠ ➥ | ${prefix}play2
+┗━━━━━━━━━━━━━
+
+┏━━━「Outros」
+┠ ➥ | ${prefix}wait
+┠ ➥ | ${prefix}info
+┠ ➥ | ${prefix}dono
+┠ ➥ | ${prefix}meme
+┠ ➥ | ${prefix}gtts
+┠ ➥ | ${prefix}ler
+┗━━━━━━━━━━━━━
+
+
+`
+}
+
+exports.help = help
